184e0cd346693148b7e55d3c2e60cf58abb532a1
Gavin Popken <gavinpopkenart@gmail.com>
2024-01-11 10:36:57 -0600

    finished get route in the shopping router, set up shopping router

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const shoppingRouter = require('./routes/shopping.router')
 const PORT = process.env.PORT || 5001;
 
 /** ---------- MIDDLEWARE ---------- **/
@@ -8,6 +9,7 @@ app.use(bodyParser.json()); // needed for axios requests
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
+app.use('/api/shopping', shoppingRouter)
 // Create your API routes in a separate file
 // and plug them in here with `app.use()`
 
