dd46f8d2f8a301adc6182dc45a856f6ad16e9d04
Michael Jancik-Kitowski <michael.jancik24@gmail.com>
2024-01-11 11:07:40 -0600

    Got the hardcoded HTML working and set up the base GET request set up

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,46 @@
 import React from 'react';
-
+import { useEffect, useState } from 'react';
 import Header from '../Header/Header.jsx'
 import './App.css';
+import axios from 'axios'
 
 
 function App() {
+    const [shoppingList, setShoppingList] = useState([])
+
+    const getShoppingList = () => {
+        axios({
+            method: 'GET',
+            url: ''
+        })
+        .then( (response) => {
+            console.log('Inside of GET', response);
+            console.log('Data check', response.data);
+            setShoppingList(response.data)
+        })
+        .catch( function(error) {
+            console.log('Error on get:', error);
+        })
+    }
+    useEffect(() => {
+        getShoppingList();
+    }, [])
+
     return (
         <div className="App">
             <Header />
             <main>
-                <p>Under Construction...</p>
+                <h2>Add Item</h2>
+                <button>Save</button>
+                <h2>Shopping List</h2>
+                <button>Reset</button>
+                <button>Clear</button>
+                <div className='item'>
+                    <h3>Apples</h3>
+                    <p>1 Bushel</p>
+                    <button>Buy</button>
+                    <button>Delete</button>
+                </div>
             </main>
         </div>
     );
